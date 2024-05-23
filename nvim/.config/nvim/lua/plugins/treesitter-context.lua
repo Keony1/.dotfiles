d7cd5f3b2f59ef8c978074b3434eb57261d3362d
Keony Schröer <keony.schroer@gmail.com>
2024-05-23 10:01:51 -0300

    add treesitter cxt

---
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = {
+            mode = "cursor",
+            max_lines = 0,
+            show_all_context = false,
+        },
+    },
+}
