092d3a29abc237ba704bac2775a9f0584fefc1ec
Keony Schröer <keony.schroer@gmail.com>
2025-01-23 18:35:39 -0300

    Maybe revert this commit?

--- nvim/.config/nvim/lua/configs/opts.lua
+++ nvim/.config/nvim/lua/configs/opts.lua
@@ -3,13 +3,16 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.wo.colorcolumn = "80"
 -- indentation
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
 -- searching
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 -- splitting
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+-- file
+vim.opt.eol = false
+vim.opt.fixeol = false
--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -1,10 +1,8 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        opts = {
-            mode = "cursor",
-            max_lines = 0,
-            show_all_context = false,
-        },
-    },
+      "nvim-treesitter/nvim-treesitter-context",
+      opts = {
+          mode = "cursor",
+          max_lines = 0,
+          show_all_context = false,
+      },
 }
